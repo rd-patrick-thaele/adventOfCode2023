6aa5b505d77d8697c9e1a4e6d2ad5a2b1cbd7c47
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-12-01 08:52:54 +0100

    :sparkles:  Day01 part 2

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,6 +1,15 @@
 class Day01 {
 
-    val regex = "([0-9]).+([0-9]?)".toRegex()
+    val calibrationValueMap = mapOf(Pair("0", 0), Pair("zero", 0),
+        Pair("1", 1), Pair("one", 1),
+        Pair("2", 2), Pair("two", 2),
+        Pair("3", 3), Pair("three", 3),
+        Pair("4", 4), Pair("four", 4),
+        Pair("5", 5), Pair("five", 5),
+        Pair("6", 6), Pair("six", 6),
+        Pair("7", 7), Pair("seven", 7),
+        Pair("8", 8), Pair("eight", 8),
+        Pair("9", 9), Pair("nine", 9))
 
     fun sumOfCalibrationValues(calibrationDoc: List<String>): Int {
         return calibrationDoc.sumOf { getCalibrationValue(it) }
@@ -12,5 +21,18 @@ class Day01 {
         val foundLast = calibrationString.findLastAnyOf(numbersRange)
 
         return foundFirst!!.second.toInt() * 10 + foundLast!!.second.toInt()
+    }
+
+    fun sumOfCalibrationValuesSpelled(calibrationDoc: List<String>): Int {
+        return calibrationDoc.sumOf { getCalibrationValueSpelled(it) }
+    }
+
+    fun getCalibrationValueSpelled(calibrationString: String): Int {
+        val numbersToBeFound = calibrationValueMap.keys
+
+        val foundFirst = calibrationString.findAnyOf(numbersToBeFound)
+        val foundLast = calibrationString.findLastAnyOf(numbersToBeFound)
+
+        return calibrationValueMap.getValue(foundFirst!!.second) * 10 + calibrationValueMap.getValue(foundLast!!.second)
     }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -35,18 +35,73 @@ class Day01Test : FreeSpec({
             // then
             sumOfCalibrationValues shouldBe 142
         }
+
+        "getCalibrationValueSpelled - spelled at beginning and end" {
+            Day01().getCalibrationValueSpelled("two1nine") shouldBe 29
+        }
+
+        "getCalibrationValueSpelled - spelled only" {
+            Day01().getCalibrationValueSpelled("eightwothree") shouldBe 83
+        }
+
+        "getCalibrationValueSpelled - spelled with suffix and prefix" {
+            Day01().getCalibrationValueSpelled("abcone2threexyz") shouldBe 13
+        }
+
+        "getCalibrationValueSpelled - spelled mixed" {
+            Day01().getCalibrationValueSpelled("xtwone3four") shouldBe 24
+        }
+
+        "getCalibrationValueSpelled - digits first" {
+            Day01().getCalibrationValueSpelled("4nineeightseven2") shouldBe 42
+        }
+
+        "getCalibrationValueSpelled - spelled and digit" {
+            Day01().getCalibrationValueSpelled("zoneight234") shouldBe 14
+        }
+
+        "getCalibrationValueSpelled - ignore teens" {
+            Day01().getCalibrationValueSpelled("7pqrstsixteen") shouldBe 76
+        }
+
+        "part 2" - {
+            // given
+            val calibrationDoc = """
+                two1nine
+                eightwothree
+                abcone2threexyz
+                xtwone3four
+                4nineeightseven2
+                zoneight234
+                7pqrstsixteen
+            """.trimIndent().lineSequence().toList()
+
+            // when
+            val sumOfCalibrationValues = Day01().sumOfCalibrationValuesSpelled(calibrationDoc)
+
+            // then
+            sumOfCalibrationValues shouldBe 281
+        }
     }
 
     "solution" - {
-        "part 1" {
-            // given
-            val calibrationDoc = getResourceFileAsStringSequence("day01/input.txt")
+        // given
+        val calibrationDoc = getResourceFileAsStringSequence("day01/input.txt")
 
+        "part 1" {
             // when
             val sumOfCalibrationValues = Day01().sumOfCalibrationValues(calibrationDoc)
 
             // then
             sumOfCalibrationValues shouldBe 55386
+        }
+
+        "part 2" {
+            // when
+            val sumOfCalibrationValues = Day01().sumOfCalibrationValuesSpelled(calibrationDoc)
+
+            // then
+            sumOfCalibrationValues shouldBe 54824
         }
     }
 }) {
