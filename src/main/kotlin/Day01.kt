525d4bd40a32c6b30e566972cf801c30a897f349
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-12-01 08:31:52 +0100

    :construction:  Day01 part 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,6 +1,16 @@
 class Day01 {
 
-    fun getHelloWorld(): String {
-        return "Hello World"
+    val regex = "([0-9]).+([0-9]?)".toRegex()
+
+    fun sumOfCalibrationValues(calibrationDoc: List<String>): Int {
+        return calibrationDoc.sumOf { getCalibrationValue(it) }
+    }
+
+    fun getCalibrationValue(calibrationString: String): Int {
+        val numbersRange = (0..9).toList().map { it.toString() }
+        val foundFirst = calibrationString.findAnyOf(numbersRange)
+        val foundLast = calibrationString.findLastAnyOf(numbersRange)
+
+        return foundFirst!!.second.toInt() * 10 + foundLast!!.second.toInt()
     }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -4,19 +4,49 @@ import io.kotest.matchers.shouldBe
 class Day01Test : FreeSpec({
 
     "samples" - {
-        "helloWorld Test" {
-            Day01().getHelloWorld() shouldBe "Hello World"
+        "getCalibrationValue - start/beginning" {
+            Day01().getCalibrationValue("1abc2") shouldBe 12
+        }
+
+        "getCalibrationValue - middle" {
+            Day01().getCalibrationValue("pqr3stu8vwx") shouldBe 38
+        }
+
+        "getCalibrationValue - multiple digits" {
+            Day01().getCalibrationValue("a1b2c3d4e5f") shouldBe 15
+        }
+
+        "getCalibrationValue - one digit" {
+            Day01().getCalibrationValue("treb7uchet") shouldBe 77
+        }
+
+        "part 1" - {
+            // given
+            val calibrationDoc = """
+                1abc2
+                pqr3stu8vwx
+                a1b2c3d4e5f
+                treb7uchet
+            """.trimIndent().lineSequence().toList()
+
+            // when
+            val sumOfCalibrationValues = Day01().sumOfCalibrationValues(calibrationDoc)
+
+            // then
+            sumOfCalibrationValues shouldBe 142
         }
     }
 
     "solution" - {
         "part 1" {
             // given
+            val calibrationDoc = getResourceFileAsStringSequence("day01/input.txt")
 
             // when
+            val sumOfCalibrationValues = Day01().sumOfCalibrationValues(calibrationDoc)
 
             // then
-            1 + 1 shouldBe 2
+            sumOfCalibrationValues shouldBe 55386
         }
     }
 }) {
