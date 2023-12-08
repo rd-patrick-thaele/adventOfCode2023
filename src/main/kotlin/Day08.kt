6cfcdaa0c687a6948e8a588e2422f018e2cd50db
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-12-08 17:33:38 +0100

    :zap: Day08 part 2 - precalculate needed steps for one turn
    Takes about 45s now.
    
    Further improvements are possible:
     (1) The data is special: the directions and the map are designed in a way each repeat takes the same number of steps. My algorithm tries to figure out a pattern when it starts to loop. The pattern is always the same with this data set.
    
     (2) By getting the prime factors each for the steps needed to to find the target node this task can be reduced to a simple multiplication of the common prime factors.

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,3 +1,4 @@
+
 class Day08 {
 
     companion object {
@@ -49,38 +50,107 @@ class Day08 {
             .toList()
     }
 
-    fun countTotalStepsV2(input: List<String>): Int {
-        var stepCount = 0
+    fun countTotalStepsV2(input: List<String>): Long {
+        var stepCount = 0L
+        var allInSync = false
 
         val directions = input.removeFirst()
         input.removeFirst()
 
         val desertNetwork = parseDesertNetwork(input)
-        var currentNodes = getAllNodesEndingWithA(desertNetwork)
+        val patterns = getAllNodesEndingWithA(desertNetwork)
+            .map { getPattern(it, directions, desertNetwork) }
+            .toList()
 
-        while (true) {
-            for (step in directions) {
-                stepCount++
-                val nextNodes = mutableListOf<String>()
-                var allNodesFoundDestination = true
+        while (!allInSync) {
+            allInSync = true
+            for (pattern in patterns) {
+                while (pattern.stepsTaken() < stepCount) {
 
-                for (currentNode in currentNodes) {
+                    val stepsTaken = pattern.moveOn()
 
-                    val nextNode = when (step) {
-                        MOVE_LEFT -> desertNetwork[currentNode]!!.left
-                        MOVE_RIGHT -> desertNetwork[currentNode]!!.right
-                        else -> ""
+                    if (stepsTaken > stepCount) {
+                        stepCount = stepsTaken
+                        allInSync = false
                     }
-                    nextNodes.add(nextNode)
-
-                    if (!nextNode.endsWith('Z')) allNodesFoundDestination = false
                 }
-
-                if (allNodesFoundDestination) return stepCount
-                currentNodes = nextNodes
             }
         }
+
+        return stepCount
+    }
+
+    fun getPattern(startNode: String, directions: String, desertNetwork: Map<String, DesertNode>): CompletionPattern {
+
+        println("Get Pattern for startNode $startNode")
+
+        var currentNode = startNode
+        var stepsFirstFinish = 0
+        var stepsToFinish = 0
+        var firstFinishDone = false
+
+        val loopSteps = mutableListOf<Int>()
+        val directionIndexOnFinish = mutableSetOf<Int>()
+
+        while (true) {
+
+            for ((index, step) in directions.withIndex()) {
+                stepsToFinish++
+
+                val nextNode = when (step) {
+                    MOVE_LEFT -> desertNetwork[currentNode]!!.left
+                    MOVE_RIGHT -> desertNetwork[currentNode]!!.right
+                    else -> { println("ERROR: $step"); "" }
+                }
+
+                currentNode = nextNode
+
+                if (currentNode.endsWith('Z')) {
+                    if (firstFinishDone) {
+                        loopSteps.add(stepsToFinish)
+
+                        if (directionIndexOnFinish.contains(index)) {
+                            val pattern = CompletionPattern(stepsFirstFinish, loopSteps)
+                            println(pattern)
+                            return pattern
+                        }
+
+                        directionIndexOnFinish.add(index)
+                    } else {
+                        firstFinishDone = true
+                        stepsFirstFinish = stepsToFinish
+                    }
+
+                    stepsToFinish = 0
+                }
+            }
+        }
+
     }
 }
 
 data class DesertNode(val left: String, val right: String)
+
+data class CompletionPattern(val stepsFirstFinish: Int, val loop: List<Int>) {
+
+    private var stepsTaken = -1L
+    private var initialStepsDone = false
+    private var loopIndex = 0
+    fun moveOn(): Long {
+        if (!initialStepsDone) {
+            stepsTaken = stepsFirstFinish.toLong()
+            initialStepsDone = true
+        } else {
+            stepsTaken += loop[loopIndex]
+
+            if (loopIndex + 1 < loop.size) loopIndex++
+            else loopIndex = 0
+        }
+
+        return stepsTaken
+    }
+
+    fun stepsTaken(): Long {
+        return stepsTaken
+    }
+}
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -88,6 +88,67 @@ class Day08Test : FreeSpec({
             nodes shouldContainInOrder listOf("11A", "22A")
         }
 
+        "getPattern - sample 1" {
+            // given
+            val startNode = "11A"
+            val directions = "LR"
+            val desert = """
+                11A = (11B, XXX)
+                11B = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)
+            """.trimIndent().lineSequence().toList()
+            val desertNetwork = Day08().parseDesertNetwork(desert)
+
+            // when
+            val pattern = Day08().getPattern(startNode, directions, desertNetwork)
+
+            // then
+            pattern.stepsFirstFinish shouldBe 2
+            pattern.loop shouldContainInOrder listOf(2)
+        }
+
+        "getPattern - sample 2" {
+            // given
+            val startNode = "22A"
+            val directions = "LR"
+            val desert = """
+                11A = (11B, XXX)
+                11B = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)
+            """.trimIndent().lineSequence().toList()
+            val desertNetwork = Day08().parseDesertNetwork(desert)
+
+            // when
+            val pattern = Day08().getPattern(startNode, directions, desertNetwork)
+
+            // then
+            pattern.stepsFirstFinish shouldBe 3
+            pattern.loop shouldContainInOrder listOf(3, 3)
+        }
+
+        "pattern moveOn" {
+            // given
+            val pattern = CompletionPattern(2, listOf(3, 4))
+
+            // expect
+            pattern.moveOn() shouldBe 2
+            pattern.moveOn() shouldBe 5
+            pattern.moveOn() shouldBe 9
+            pattern.moveOn() shouldBe 12
+            pattern.moveOn() shouldBe 16
+            pattern.moveOn() shouldBe 19
+        }
+
         "part 2" {
             // given
             val input = """
@@ -113,10 +174,12 @@ class Day08Test : FreeSpec({
 
     "solution" - {
 
-        // given
-        val input = getResourceFileAsStringSequence("day08/input.txt")
+
 
         "part 1" {
+            // given
+            val input = getResourceFileAsStringSequence("day08/input.txt")
+
             // when
             val totalSteps = Day08().countTotalSteps(input)
 
@@ -125,11 +188,14 @@ class Day08Test : FreeSpec({
         }
 
         "part 2" {
+            // given
+            val input = getResourceFileAsStringSequence("day08/input.txt")
+
             // when
             val totalSteps = Day08().countTotalStepsV2(input)
 
             // then
-            totalSteps shouldBe 23_147
+            totalSteps shouldBe 22_289_513_667_691L
         }
     }
 })
