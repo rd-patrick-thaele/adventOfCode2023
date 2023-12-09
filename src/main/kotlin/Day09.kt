2f0545d3c4bee282ca1346266117665ecb001436
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-12-09 17:52:12 +0100

    :sparkles: Day09 part 2

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,18 +1,26 @@
 class Day09 {
-    fun predictNextValue(history: List<Int>): Int {
+    fun predictNextValue(history: List<Int>): OasisPrediction {
         if (history.none { it != 0 })
-            return 0
+            return OasisPrediction(0, 0)
 
         val diffList = mutableListOf<Int>()
         for (index in 1 until history.size) {
             diffList.add(history[index] - history[index - 1])
         }
 
-        return predictNextValue(diffList) + history.last()
+        val prediction = predictNextValue(diffList)
+        return OasisPrediction(history.first() - prediction.pastValue, prediction.futureValue + history.last())
     }
 
-    fun sumOfPredictedNextValues(report: List<String>): Int {
+    fun sumOfPredictedFutureValues(report: List<String>): Int {
         return report.map { it.split(" ").map { it.toInt() }.toList() }
-            .sumOf { predictNextValue(it) }
+            .sumOf { predictNextValue(it).futureValue }
+    }
+
+    fun sumOfPredictedPastValues(report: List<String>): Int {
+        return report.map { it.split(" ").map { it.toInt() }.toList() }
+            .sumOf { predictNextValue(it).pastValue }
     }
 }
+
+data class OasisPrediction(val pastValue: Int, val futureValue: Int)
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -1,5 +1,4 @@
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.collections.shouldContainInOrder
 import io.kotest.matchers.shouldBe
 
 class Day09Test : FreeSpec({
@@ -12,10 +11,11 @@ class Day09Test : FreeSpec({
             val history = listOf(0, 0, 0, 0, 0, 0)
 
             // when
-            val nextValue = Day09().predictNextValue(history)
+            val prediction = Day09().predictNextValue(history)
 
             // then
-            nextValue shouldBe 0
+            prediction.futureValue shouldBe 0
+            prediction.pastValue shouldBe 0
         }
 
         "predictNextValue - depth 2" {
@@ -23,10 +23,11 @@ class Day09Test : FreeSpec({
             val history = listOf(0, 3, 6, 9, 12, 15)
 
             // when
-            val nextValue = Day09().predictNextValue(history)
+            val prediction = Day09().predictNextValue(history)
 
             // then
-            nextValue shouldBe 18
+            prediction.futureValue shouldBe 18
+            prediction.pastValue shouldBe -3
         }
 
         "predictNextValue - depth 3" {
@@ -34,10 +35,11 @@ class Day09Test : FreeSpec({
             val history = listOf(1, 3, 6, 10, 15, 21)
 
             // when
-            val nextValue = Day09().predictNextValue(history)
+            val prediction = Day09().predictNextValue(history)
 
             // then
-            nextValue shouldBe 28
+            prediction.futureValue shouldBe 28
+            prediction.pastValue shouldBe 0
         }
 
         "predictNextValue - depth 4" {
@@ -45,10 +47,11 @@ class Day09Test : FreeSpec({
             val history = listOf(10, 13, 16, 21, 30, 45)
 
             // when
-            val nextValue = Day09().predictNextValue(history)
+            val prediction = Day09().predictNextValue(history)
 
             // then
-            nextValue shouldBe 68
+            prediction.futureValue shouldBe 68
+            prediction.pastValue shouldBe 5
         }
 
         "part 1" {
@@ -60,29 +63,46 @@ class Day09Test : FreeSpec({
             """.trimIndent().lineSequence().toList()
 
             // when
-            val sum = Day09().sumOfPredictedNextValues(report)
+            val sum = Day09().sumOfPredictedFutureValues(report)
 
             // then
             sum shouldBe 114
         }
+
+        "part 2" {
+            // given
+            val report = """
+                0 3 6 9 12 15
+                1 3 6 10 15 21
+                10 13 16 21 30 45
+            """.trimIndent().lineSequence().toList()
+
+            // when
+            val sum = Day09().sumOfPredictedPastValues(report)
+
+            // then
+            sum shouldBe 2
+        }
     }
 
     "solution" - {
-
+        // given
+        val report = getResourceFileAsStringSequence("day09/input.txt")
 
         "part 1" {
-            // given
-            val report = getResourceFileAsStringSequence("day09/input.txt")
-
             // when
-            val sum = Day09().sumOfPredictedNextValues(report)
+            val sum = Day09().sumOfPredictedFutureValues(report)
 
             // then
             sum shouldBe 1_993_300_041
         }
 
         "part 2" {
-            // given
+            // when
+            val sum = Day09().sumOfPredictedPastValues(report)
+
+            // then
+            sum shouldBe 1_038
         }
     }
 })
