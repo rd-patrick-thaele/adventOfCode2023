3274db36804479fa3c3e6908504dd5d7ea37130f
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-12-10 21:36:28 +0100

    :sparkles: Day10 part 2
    Inspired by polygon filling in graphics tools. See:
    https://www.cs.uic.edu/~jbell/CourseNotes/ComputerGraphics/PolygonFilling.html

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -11,6 +11,9 @@ class Day10(val map: List<String>) {
     }
 
     private var startingPosition = PipePosition(-1, -1)
+    private var startSymbolReplacement = '.'
+    private val pipePositions = mutableSetOf<PipePosition>()
+
     fun findStartPosition() {
         for ((y, line) in map.withIndex()) {
             for ((x, symbol) in line.withIndex()) {
@@ -25,17 +28,20 @@ class Day10(val map: List<String>) {
     fun stepsToOppositePosition(): Int {
         findStartPosition()
         var pipeDirection = findFirstPipeDirection()
+        pipePositions.add(startingPosition)
         var nextPosition = startingPosition
         var stepsTaken = 0
 
         while (true) {
             stepsTaken++
-            nextPosition = when(pipeDirection) {
+
+            nextPosition = when (pipeDirection) {
                 PipeDirection.NORTH -> PipePosition(nextPosition.y - 1, nextPosition.x)
                 PipeDirection.EAST -> PipePosition(nextPosition.y, nextPosition.x + 1)
                 PipeDirection.SOUTH -> PipePosition(nextPosition.y + 1, nextPosition.x)
                 PipeDirection.WEST -> PipePosition(nextPosition.y, nextPosition.x - 1)
             }
+            pipePositions.add(nextPosition)
 
             val symbol = map[nextPosition.y][nextPosition.x]
             if (symbol == STARTING_SYMBOL) {
@@ -85,17 +91,91 @@ class Day10(val map: List<String>) {
 
     fun findFirstPipeDirection(): PipeDirection {
 
+        var firstDirection:PipeDirection? = null
+
         val eastNeighbor = map[startingPosition.y][startingPosition.x + 1]
         if (eastNeighbor in listOf(WEST_EAST_SYMBOL, NORTH_WEST_SYMBOL, SOUTH_WEST_SYMBOL)) {
-            return PipeDirection.EAST
+            firstDirection =  PipeDirection.EAST
         }
 
         val southNeighbor = map[startingPosition.y + 1][startingPosition.x]
         if (southNeighbor in listOf(NORTH_SOUTH_SYMBOL, NORTH_EAST_SYMBOL, NORTH_WEST_SYMBOL)) {
-            return PipeDirection.SOUTH
+            if (firstDirection != null) {
+                startSymbolReplacement = SOUTH_EAST_SYMBOL
+                return firstDirection
+            }
+            firstDirection = PipeDirection.SOUTH
         }
 
-        return PipeDirection.WEST
+        val westNeighbor = map[startingPosition.y][startingPosition.x - 1]
+        if (westNeighbor in listOf(NORTH_EAST_SYMBOL, WEST_EAST_SYMBOL, SOUTH_EAST_SYMBOL)) {
+            if (firstDirection == null) {
+                startSymbolReplacement = NORTH_WEST_SYMBOL
+                return PipeDirection.WEST
+            }
+            when(firstDirection) {
+                PipeDirection.EAST -> startSymbolReplacement = WEST_EAST_SYMBOL
+                PipeDirection.SOUTH -> startSymbolReplacement = SOUTH_WEST_SYMBOL
+                else -> println("Won't reach this")
+            }
+
+            return firstDirection
+        }
+
+        when(firstDirection) {
+            PipeDirection.EAST -> startSymbolReplacement = NORTH_EAST_SYMBOL
+            PipeDirection.SOUTH -> startSymbolReplacement = NORTH_SOUTH_SYMBOL
+            else -> println("Won't reach this")
+        }
+
+        return firstDirection!!
+    }
+
+    fun getEnclosedTiles(): Int {
+        stepsToOppositePosition()
+        var enclosedTiles = 0
+        var edgeParity: Boolean
+        val intersectionSymbols = mutableSetOf<Char>()
+
+        val replacedString = map[startingPosition.y].replace('S', startSymbolReplacement)
+        val updatedMap = map.toMutableList()
+        updatedMap[startingPosition.y] = replacedString
+
+        for ((y, line) in updatedMap.withIndex()) {
+            edgeParity = true
+            intersectionSymbols.clear()
+
+            for ((x, symbol) in line.withIndex()) {
+
+                if (pipePositions.contains(PipePosition(y, x))) {
+                    if (symbol == WEST_EAST_SYMBOL) continue
+
+                    //intersections: L-7, |, F-J
+                    if (symbol in listOf(NORTH_EAST_SYMBOL, NORTH_WEST_SYMBOL, SOUTH_EAST_SYMBOL, SOUTH_WEST_SYMBOL)) {
+                        intersectionSymbols.add(symbol)
+                    }
+
+                    if (symbol == NORTH_SOUTH_SYMBOL
+                        || intersectionSymbols.containsAll(listOf(NORTH_EAST_SYMBOL, SOUTH_WEST_SYMBOL))
+                        || intersectionSymbols.containsAll(listOf(SOUTH_EAST_SYMBOL, NORTH_WEST_SYMBOL))
+                    ) {
+                        edgeParity = !edgeParity
+                        intersectionSymbols.clear()
+                    }
+
+                    if (intersectionSymbols.size > 1)
+                        intersectionSymbols.clear()
+
+                    continue
+                }
+
+                if (!edgeParity) {
+                    enclosedTiles++
+                }
+            }
+        }
+
+        return enclosedTiles
     }
 }
 
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -78,13 +78,82 @@ class Day10Test : FreeSpec({
             // then
             steps shouldBe 8
         }
+
+        "part 2 - sample 1" {
+
+            val map = """
+                ...........
+                .S-------7.
+                .|F-----7|.
+                .||.....||.
+                .||.....||.
+                .|L-7.F-J|.
+                .|..|.|..|.
+                .L--J.L--J.
+                ...........
+            """.trimIndent().lineSequence().toList()
+
+            // when
+            val enclosedTiles = Day10(map).getEnclosedTiles()
+
+            // then
+            enclosedTiles shouldBe 4
+
+        }
+
+        "part 2 - sample 2" {
+            // given
+            val map = """
+                .F----7F7F7F7F-7....
+                .|F--7||||||||FJ....
+                .||.FJ||||||||L7....
+                FJL7L7LJLJ||LJ.L-7..
+                L--J.L7...LJS7F-7L7.
+                ....F-J..F7FJ|L7L7L7
+                ....L7.F7||L7|.L7L7|
+                .....|FJLJ|FJ|F7|.LJ
+                ....FJL-7.||.||||...
+                ....L---J.LJ.LJLJ...
+            """.trimIndent().lineSequence().toList()
+
+            // when
+            val enclosedTiles = Day10(map).getEnclosedTiles()
+
+            // then
+            enclosedTiles shouldBe 8
+
+        }
+
+        "part 2 - sample 3" {
+            // given
+            val map = """
+                FF7FSF7F7F7F7F7F---7
+                L|LJ||||||||||||F--J
+                FL-7LJLJ||||||LJL-77
+                F--JF--7||LJLJ7F7FJ-
+                L---JF-JLJ.||-FJLJJ7
+                |F|F-JF---7F7-L7L|7|
+                |FFJF7L7F-JF7|JL---7
+                7-L-JL7||F7|L7F-7F7|
+                L.L7LFJ|||||FJL7||LJ
+                L7JLJL-JLJLJL--JLJ.L
+            """.trimIndent().lineSequence().toList()
+
+            // when
+            val enclosedTiles = Day10(map).getEnclosedTiles()
+
+            // then
+            enclosedTiles shouldBe 10
+        }
     }
 
     "solution" - {
 
+        // given
+        val map = getResourceFileAsStringSequence("day10/input.txt")
+
         "part 1" {
             //given
-            val map = getResourceFileAsStringSequence("day10/input.txt")
             val day = Day10(map)
 
             // when
@@ -96,6 +165,10 @@ class Day10Test : FreeSpec({
 
         "part 2" {
             // when
+            val enclosedTiles = Day10(map).getEnclosedTiles()
+
+            // then
+            enclosedTiles shouldBe 429
         }
     }
 })
